0cb2fd1a9ffb8979f72f3eab627179084a9546cf
Asher <asher.grossman@gmail.com>
2024-01-19 13:17:13 -0600

    Update attendance admin user card UI and add date formatting functionality.
    - Updated the UI of the user card in the attendance admin.
    - Added functionality to format dates using the `intl` package.
    - Imported necessary packages for file handling, date formatting, and cloud integration.

--- lib/user_card.dart
+++ lib/user_card.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 class UserCard extends StatefulWidget {
   final String uid;
@@ -18,13 +22,14 @@ class _UserCardState extends State<UserCard> {
   int? totalSessions;
   int? totalTimeMilliseconds;
   String? totalTime;
+  DateTime? lastEventTime;
 
   @override
   void initState() {
     super.initState();
     uid = widget.uid;
-
     reload();
+    listenForChange();
   }
 
   @override
@@ -45,7 +50,7 @@ class _UserCardState extends State<UserCard> {
                     ? Colors.black
                     : Theme.of(context).colorScheme.onSurface),
             child: ExpansionTile(
-              title: Text(name ?? 'Loading...',
+              title: Text(generateTitle(),
                   style: TextStyle(
                       color: (userState != null && userState == 'in')
                           ? Colors.black
@@ -55,6 +60,15 @@ class _UserCardState extends State<UserCard> {
                 Text('Sessions: $totalSessions'),
                 Text('State: $userState'),
                 Text('Total Time: $totalTime'),
+                Visibility(
+                    visible: userState == 'in',
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          await FirebaseFunctions.instance
+                              .httpsCallable('uidSignOut')
+                              .call({"uid": uid});
+                        },
+                        child: const Text('Stop time')))
               ],
             ),
           ),
@@ -63,11 +77,29 @@ class _UserCardState extends State<UserCard> {
     );
   }
 
-  // void listenForChange() {
-  //   FirebaseDatabase.instance.ref('$uid/state').onValue.listen((DatabaseEvent event) {
-  //     reload();
-  //   });
-  // }
+  String generateTitle() {
+    if (name == null) return 'Loading...';
+    String wrappedFormattedDate = '';
+    if (userState == 'in') {
+      String formattedDate =
+          DateFormat('h:mm a, d MMMM yyyy').format(lastEventTime!);
+      wrappedFormattedDate = '(Checked in: $formattedDate)';
+    } else {
+      String formattedDate =
+          DateFormat('h:mm a, d MMMM yyyy').format(lastEventTime!);
+      wrappedFormattedDate = '(Checked out: $formattedDate)';
+    }
+    return '$name $wrappedFormattedDate';
+  }
+
+  void listenForChange() {
+    FirebaseDatabase.instance
+        .ref('$uid/state')
+        .onValue
+        .listen((DatabaseEvent event) {
+      reload();
+    });
+  }
 
   void reload() async {
     DataSnapshot snapshot = await ref.child(uid).get();
@@ -81,6 +113,19 @@ class _UserCardState extends State<UserCard> {
           '${totalTimeDuration.inHours}:${totalTimeDuration.inMinutes.remainder(60).toString().padLeft(2, '0')}:${totalTimeDuration.inSeconds.remainder(60).toString().padLeft(2, '0')}';
       totalSessions = snapshot.child('totalSessions').value as int;
       userState = snapshot.child('state').value.toString();
+      int counter = snapshot.child('counter').value as int;
+      if (counter == 0) {
+        lastEventTime = DateTime.fromMillisecondsSinceEpoch(0);
+      } else {
+        if (userState == 'in') {
+          int lastIn = snapshot.child('sessions/$counter/timeIn').value as int;
+          lastEventTime = DateTime.fromMillisecondsSinceEpoch(lastIn);
+        } else {
+          int lastOut =
+              snapshot.child('sessions/${counter - 1}/timeOut').value as int;
+          lastEventTime = DateTime.fromMillisecondsSinceEpoch(lastOut);
+        }
+      }
     });
   }
 }
