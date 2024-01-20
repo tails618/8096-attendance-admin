a9152b268ded98792879ea9cc4f2d3564d8e02b4
Asher <asher.grossman@gmail.com>
2024-01-20 01:25:13 -0600

    deploy

--- .firebase/8096-attendance-admin/hosting/flutter_service_worker.js
+++ .firebase/8096-attendance-admin/hosting/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "85c88b43ea6d3a9af7193ee6e0e59653",
-"index.html": "89103021e2af700687e2e1f087a86782",
-"/": "89103021e2af700687e2e1f087a86782",
-"main.dart.js": "14e1bbd0470f716738ad65a4cb1e0c02",
+"index.html": "0533f94c632c97c7fa74c42a12d81915",
+"/": "0533f94c632c97c7fa74c42a12d81915",
+"main.dart.js": "2085f1480af0f254bf0479269208e805",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
